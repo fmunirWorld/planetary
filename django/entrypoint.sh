478a88f46ead9c1a60d340c6d3433d8418ea0c58
Fahad Munir <fmunir923@gmail.com>
2020-04-07 01:02:00 +0500

    Upgrade Django to 3 ASGI

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Prepare log files and start outputting logs to stdout
-# mkdir -p /code/logs
-# touch /code/logs/gunicorn.log
-# touch /code/logs/gunicorn-access.log
-# tail -n 0 -f /code/logs/gunicorn*.log &
-
 export DJANGO_SETTINGS_MODULE=planetary.settings
 
-exec gunicorn planetary.wsgi:application \
-     --name planetary \
-     --bind 0.0.0.0:8000 \
-     --workers 4 \
-    #  --log-level=info \
-    #  --log-file=/code/logs/gunicorn.log \
-    #  --access-logfile=/code/logs/gunicorn-access.log \
+exec uvicorn planetary.asgi:application \
+     --host 0.0.0.0 \
+     --port 8000 \
+    #  --workers 4 \
 "$@"
